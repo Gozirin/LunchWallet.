3d984f6f1d5a3a6f451941418b657fd080375d29
Organizer <organizersamuel.os@gmail.com>
2022-07-05 10:01:14 +0100

    Launch wallet MVI Architecture Done

--- app/src/main/java/com/example/lunchwallet/MainActivity.kt
+++ app/src/main/java/com/example/lunchwallet/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.lunchwallet
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lunchwallet.databinding.ActivityMainBinding
+import com.example.lunchwallet.ui.views.MainFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(binding.container.id, MainFragment.newInstance())
+                .commit()
+        }
     }
 }
