47bc035d635ece0b9abb3a6415145b2c5c6444bb
Organizer <organizersamuel.os@gmail.com>
2022-07-07 00:51:16 +0100

    launch wallet landing page done v2

--- app/src/main/java/com/example/lunchwallet/MainActivity.kt
+++ app/src/main/java/com/example/lunchwallet/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.lunchwallet
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lunchwallet.databinding.ActivityMainBinding
-import com.example.lunchwallet.ui.views.LandingPage
+import com.example.lunchwallet.ui.views.MainFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
-                .add(binding.container.id, LandingPage.newInstance())
+                .add(binding.container.id, MainFragment.newInstance())
                 .commit()
         }
     }
--- app/src/main/java/com/example/lunchwallet/ui/views/MainFragment.kt
+++ app/src/main/java/com/example/lunchwallet/ui/views/MainFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.lunchwallet.MainActivity
 import com.example.lunchwallet.R
+import com.example.lunchwallet.databinding.MainFragmentBinding
 import com.example.lunchwallet.ui.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
@@ -16,18 +18,32 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(LayoutInflater.from(container?.context), container, false)
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.goToLandingPageBtn.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.container, LandingPage.newInstance())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
