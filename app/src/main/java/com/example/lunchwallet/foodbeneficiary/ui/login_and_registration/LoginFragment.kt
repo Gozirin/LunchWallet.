479b7aa921e8320c3ba7015c64ac60008ee64b7a
Gozirin <chigozp2@gmail.com>
2022-07-08 21:47:57 +0100

    lunch_wallet_login_page

---
+++ app/src/main/java/com/example/lunchwallet/foodbeneficiary/ui/login_and_registration/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.example.lunchwallet.foodbeneficiary.ui.login_and_registration
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lunchwallet.R
+
+class LoginFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_login)
+    }
+}
