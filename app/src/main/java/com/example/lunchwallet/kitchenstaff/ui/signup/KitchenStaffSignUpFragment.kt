89c57dc5a2d07c511c8d1e0a4233b8598b08c65b
Organizer <organizersamuel.os@gmail.com>
2022-07-26 12:17:01 +0100

    Kitchen staff signup done

---
+++ app/src/main/java/com/example/lunchwallet/kitchenstaff/ui/signup/KitchenStaffSignUpFragment.kt
@@ -0,0 +1,53 @@
+package com.example.lunchwallet.kitchenstaff.ui.signup
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.lunchwallet.R
+import com.example.lunchwallet.databinding.FragmentKitchenStaffSignUpBinding
+
+class KitchenStaffSignUpFragment : Fragment() {
+    private lateinit var binding: FragmentKitchenStaffSignUpBinding
+
+    companion object {
+        fun newInstance() = KitchenStaffSignUpFragment()
+    }
+
+    private lateinit var viewModel: KitchenStaffSignUpViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentKitchenStaffSignUpBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this)[KitchenStaffSignUpViewModel::class.java]
+        // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        locationDropDown()
+    }
+
+    private fun locationDropDown() {
+        val location = resources.getStringArray(R.array.location)
+        val adapter = ArrayAdapter(requireContext(), R.layout.location_list, location)
+        adapter.setNotifyOnChange(true)
+        with(binding.signUpLocationDropdown) {
+            setAdapter(adapter)
+//            setOnItemClickListener { adapterView, view, i, l ->
+//
+//            }
+        }
+    }
+
+}
